0faf0c632205938d2db3af050bf5d5e7f62b2227
FadyFaragallah <fadyfaragallah@gmail.com>
2019-04-25 22:25:36 +0200

    fixing the ocr to send image as base64

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import requests
 import json
+import cv2
+import base64
+from io import BytesIO
+from PIL import Image
+
 class OCRSpaceLanguage:
     Arabic = 'ara'
     English = 'eng'
 
 class OCRSpace:
-    def __init__(self, api_key, language=OCRSpaceLanguage.English):
+    def __init__(self, api_key,string_file, language=OCRSpaceLanguage.English):
         """ ocr.space API wrapper
         :param api_key: API key string
         :param language: document language
@@ -16,6 +21,7 @@ class OCRSpace:
             'isOverlayRequired': True,
             'apikey': self.api_key,
             'language': self.language,
+            'base64Image':'data:image/jpeg;base64,'+string_file,
         }
 
     def ocr_file(self, filename):
@@ -31,15 +37,38 @@ class OCRSpace:
             )
         return r.json()
 
+    def ocr_string(self):
+        """ OCR.space API request with local file
+        :param filename: Your file path & name
+        :return: Result in JSON format
+        """
+        
+        r = requests.post(
+                'https://api.ocr.space/parse/image',
+                data=self.payload
+            )
+        return r.json()
+
 
 API_KEY = 'e9925d2b1f88957'
 
-ocr = OCRSpace(API_KEY,OCRSpaceLanguage.Arabic)
-results = ocr.ocr_file('./card.jpg')
-lines_arr = results['ParsedResults'][0]['TextOverlay']['Lines']
 
+im = cv2.imread('card_out.jpg')
+pil_img = Image.fromarray(im)
+buff = BytesIO()
+pil_img.save(buff, format="JPEG")
+new_image_string = base64.b64encode(buff.getvalue()).decode("utf-8")
+#results = ocr.ocr_file('./card_out.jpg')
+ocr = OCRSpace(API_KEY,new_image_string,OCRSpaceLanguage.Arabic)
+results = ocr.ocr_string()
+lines_arr = results['ParsedResults'][0]['TextOverlay']['Lines']
+#words and locations array --> word , top , left
+words_locations =[]
 for i in range(len(lines_arr)):
     words_arr = lines_arr[i]['Words']
     for j in range(len(words_arr)):
+        current = words_arr[j] 
         print words_arr[j]
         print words_arr[j]['WordText'].encode('utf-8')
+        words_locations.append([current['WordText'].encode('utf-8'),current['Top'],current['Left']])
+print words_locations
